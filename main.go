93a5d86833a579a8dcd11cc0114f25b8daa2e638
Mark Taylor <mtaylor@redventures.com>
2019-01-20 17:50:44 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,44 @@
+//This is a basic webserver written in Go
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<doctype html>
+<html>
+					<head>
+								<title>Hello Gopher</title>
+					</head>
+					<body>
+								Hello Gopher </br>
+								It is really awesome that Docker is written in Go!
+					</body>
+</html>`,
+				)
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprint(w, "Go web app powered by Docker")
+}
+
+func main () {
+				http.HandleFunc("/", defaultHandler)
+				http.HandleFunc("/hello", helloHandler)
+				err := http.ListenAndServe(":8080", nil)
+				if err != nil {
+								log.Fatal("ListenAndServe: ", err)
+								return
+				}
+}

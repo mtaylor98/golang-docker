59a972c6ec0c8f3afad22bcd4be05fcd7a386984
Mark Taylor <mtaylor@redventures.com>
2019-01-21 11:36:06 -0500

    Add Trey Face with /trey path

--- main.go
+++ main.go
@@ -33,9 +33,29 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprint(w, "Go web app powered by Docker")
 }
 
+func treyHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<doctype html>
+<html>
+				<head>
+							<title>Trey Face</title>
+				</head>
+				<body>
+							<img src="https://i.imgur.com/l3SzCI5.jpg?1">
+				</body>
+</html>`,
+				)
+}
+
 func main () {
 				http.HandleFunc("/", defaultHandler)
 				http.HandleFunc("/hello", helloHandler)
+				http.HandleFunc("/trey", treyHandler)
 				err := http.ListenAndServe(":8080", nil)
 				if err != nil {
 								log.Fatal("ListenAndServe: ", err)
